b7a09e04758650b367510804f55c674def4419c4
thegodcoder11 <140531012+thegodcoder11@users.noreply.github.com>
2026-01-04 17:11:52 +0200

    9g8

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let secret = document.getElementsByClassName("secretlink")[0];
 secret.addEventListener('mouseenter', function () {
-    secret.style.left = Math.random() * 100 + '%';
-    secret.style.top = Math.random() * 100 + '%';
+    secret.style.left = Math.random() * 85 + '%';
+    secret.style.top = Math.random() * 97 + '%';
     secret.style.position = 'absolute';
 });
 let nanoman = document.getElementById("nanoman")
@@ -18,14 +18,25 @@ console.log(nanomanresetbutton);
 nanomanresetbutton.addEventListener('click', function () {
     console.log('click me')
     nanoman.innerHTML = 'click me'
-    nanomanresetbutton.style.backgroundColor = '#3e9999'
+    nanomanresetbutton.style.backgroundColor = makeRandomColor()
+    nanomanresetbutton.style.color = makeRandomColor2()
 })
 
 function makeRandomColor() {
 
-    let r = Math.floor(Math.random() * 0)
+    let r = Math.floor(Math.random() * 1)
     let g = Math.floor(Math.random() * 256)
     let b = Math.floor(Math.random() * 255)
-let color = `rgb (${r}, ${g}, ${b})`
+let color = `rgb(${r}, ${g}, ${b})`
 return color
+
+}
+function makeRandomColor2() {
+
+    let r = Math.floor(Math.random() * 256)
+    let g = Math.floor(Math.random() * 256)
+    let b = Math.floor(Math.random() * 2)
+let color = `rgb(${r}, ${g}, ${b})`
+return color
+
 }
